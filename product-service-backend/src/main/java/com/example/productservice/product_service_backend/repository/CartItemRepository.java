6e187c7aa760f596257e2bc9a3cfb231cac9109e
Vedant Mete <99560888+VedantMete@users.noreply.github.com>
2025-08-07 12:01:33 +0530

    Delete product-service-backend directory

--- product-service-backend/src/main/java/com/example/productservice/product_service_backend/repository/CartItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.productservice.product_service_backend.repository;
-
-import com.example.productservice.product_service_backend.entity.CartItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CartItemRepository extends JpaRepository<CartItem, Long> {
-}
